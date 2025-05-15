9b3526e1e7daf6d8555550726e9a8c596d725329
bonsthie <barnabe.bonnet@gmail.com>
2025-05-15 13:02:45 +0200

    fix the path of sysdeps

--- config/template/sysdeps.mk
+++ config/template/sysdeps.mk
@@ -1,6 +1,6 @@
 # $(1) sysdeps path
 # $(2) SRC_DEST
 define GET_SRC_SYSDEPS
-  $(eval __SYSDEPS_SRC_PATH := $(realpath $(1)/$(SYSTEM)/$(ARCH)))
+  $(eval __SYSDEPS_SRC_PATH := $(1)/$(SYSTEM)/$(ARCH))
   $(eval $(2) := $(shell find $(__SYSDEPS_SRC_PATH)  \( -name "*.c" -o -name "*.S" \)))
 endef
